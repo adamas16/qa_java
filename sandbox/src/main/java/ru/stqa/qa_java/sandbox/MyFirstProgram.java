a02abe0b7d8c80a16e889dcf31bb60c7a8e5e0ba
adamas16 <adamas16@mail.ru>
2019-12-22 16:42:06 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/qa_java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/qa_java/sandbox/MyFirstProgram.java
@@ -3,8 +3,27 @@ package ru.stqa.qa_java.sandbox;
 public class MyFirstProgram{
 
 	public static void main (String[] args){
-		
-		System.out.println("Hello, world!");
-
+		hello("people");
+		hello("world");
+		hello("me");
+		//System.out.println("Hello, world!");
+		double l = 156;
+		System.out.println("Площадь квадрата со стороной " + l + " =" + area(l));
+		double a = 151;
+		double b = 435;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " =" + area(a,b));
 	}
+
+	public static void hello (String somebody){
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double len){
+		return len * len;
+	}
+
+	public static double area (double a, double b){
+		return a * b;
+	}
+
 }
