cffc4ac13a0324ca8fc6466ec2881ac5a6eef1f6
adamas16 <adamas16@mail.ru>
2019-12-22 17:18:02 +0300

    Создание классов

--- sandbox/src/main/java/ru/stqa/qa_java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/qa_java/sandbox/MyFirstProgram.java
@@ -7,23 +7,24 @@ public class MyFirstProgram{
 		hello("world");
 		hello("me");
 		//System.out.println("Hello, world!");
-		double l = 156;
-		System.out.println("Площадь квадрата со стороной " + l + " =" + area(l));
-		double a = 151;
-		double b = 435;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " =" + area(a,b));
+
+		Square s = new Square(156);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+		Rectangle r = new Rectangle(548,789);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b + " = " + area(r));
 	}
 
 	public static void hello (String somebody){
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area (double len){
-		return len * len;
+	public static double area (Square s){
+		return s.l * s.l;
 	}
 
-	public static double area (double a, double b){
-		return a * b;
+	public static double area (Rectangle r){
+		return r.a * r.b;
 	}
 
 }
---
+++ sandbox/src/main/java/ru/stqa/qa_java/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.qa_java.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public Square (double l){
+        this.l = l;
+    }
+
+
+}
